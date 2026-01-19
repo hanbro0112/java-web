25e5d1fbdd4c9ca71dd82abe63cf1da5dc29904e
Henry <xx2468xx@gmail.com>
2026-01-19 17:39:32 +0800

    feat: pom.xml setting

---
+++ sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -0,0 +1,17 @@
+package com.sky;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@EnableTransactionManagement //開啟註解方式的事務管理
+@Slf4j
+public class SkyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkyApplication.class, args);
+        log.info("server started...");
+    }
+}
