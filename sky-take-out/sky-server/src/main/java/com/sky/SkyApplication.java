f02201be14b803e043e04995591d32226c7c6675
Henry <xx2468xx@gmail.com>
2026-01-22 16:37:48 +0800

    fix: module startup setting

--- sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootApplication
 @EnableTransactionManagement //開啟註解方式的事務管理
-@Slf4j
 public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
